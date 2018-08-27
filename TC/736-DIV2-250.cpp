ea34b6cf52dae8bfaf03b4649d4a66766032a7ab
aajjbb <jefersonlsiq@gmail.com>
2018-08-27 01:14:40 -0300

    add 250/500 problems from TC736-DIV2

---
+++ TC/736-DIV2-250.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+class A0Paper {
+public:
+	string canBuild(vector <int> A) {
+		const int N = A.size();
+
+		for (int i = N - 2; i >= 0; i--) {
+			A[i] += A[i + 1] / 2;
+			A[i + 1] = A[i + 1] % 2;
+		}
+
+		if (A[0] > 0) {
+			return "Possible";
+		} else {
+			return "Impossible";
+		}
+	}
+};
+
+//Powered by KawigiEdit 2.1.4 (beta) modified by pivanof!
---
+++ TC/736-DIV2-500.cpp
@@ -0,0 +1,63 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+class Reroll {
+public:
+	int minimumDice(int target, vector <int> dice) {
+		const int N = dice.size();
+		int curr_sum = accumulate(dice.begin(), dice.end(), 0);
+		int best_case = curr_sum;
+		int reroll = 0;
+
+		if (target == curr_sum) {
+			return 0;
+		}
+
+		sort(dice.begin(), dice.end());
+
+		if (curr_sum > target) {
+			reverse(dice.begin(), dice.end());
+		}
+
+		for (int i = 0; i < N; i++) {
+			best_case -= dice[i];
+
+			if (curr_sum > target) {
+				best_case += 1;
+			} else if (curr_sum < target) {
+				best_case += 6;
+			}
+
+			reroll += 1;
+
+			if ((curr_sum < target && best_case >= target) ||
+				(curr_sum > target && best_case <= target)) {
+				break;
+			}
+		}
+
+		return reroll;
+	}
+};
+
+//Powered by KawigiEdit 2.1.4 (beta) modified by pivanof!
