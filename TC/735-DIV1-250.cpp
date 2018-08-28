c748b780c3371dd6aa48454d3dc7c4c35be9fd1a
aajjbb <jefersonlsiq@gmail.com>
2018-08-28 19:00:25 -0300

    add TC 735-DIV1-250 solution

---
+++ TC/735-DIV1-250.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+class PalindromeSubsequence {
+public:
+	bool isPal(const string arg) {
+		for (int i = 0; i < arg.size(); i++) {
+			if (arg[i] != arg[arg.size() - i - 1]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	vector <int> optimalPartition(string s) {
+		const int N = (int) s.size();
+		vector<int> ans(N, 1);
+
+		if (!isPal(s)) {
+			for (int i = 0; i < N; i++) {
+				if (s[i] == 'b') {
+					ans[i] = 2;
+				}
+			}
+		}
+
+		return ans;
+	}
+};
+
+//Powered by KawigiEdit 2.1.4 (beta) modified by pivanof!
